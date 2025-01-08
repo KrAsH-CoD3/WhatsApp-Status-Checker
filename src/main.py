e75ef46acd628541aaf58c598ea753c4ee0f1b5d
krashed <44311301+KrAsH-CoD3@users.noreply.github.com>
2025-01-08 09:08:11 +0100

    Refactor and updated outdated xpaths

--- src/main.py
+++ src/main.py
@@ -25,7 +25,7 @@ Enter "Y" to get notified or "N" to view them automatically: ')
 gmtTime: str = lambda tz: datetime.now(
     pytz.timezone(tz)).strftime("%I:%M:%S %p")
 
-checkStatus = lambda : bot.find_element(By.XPATH, ppsXpath)  # Status Circle around profile picture
+checkStatus = lambda : bot.find_element(By.XPATH, pps_xpath)  # Status Circle around profile picture
 
 def reminderFn(ttime_diff: float, sstart: float) -> float:
     if (
@@ -44,13 +44,15 @@ def getNotified() -> None:
             time_diff = float("{:.2f}".format(perf_counter())) - start
             
             if time_diff <= 0.2:
-                tprint(f"\n{statusUploaderName} has a status.\n{gmtTime(timezone)}")
-                wa_bot.send_message(NUMBER, f"{statusUploaderName} has a status.\n{gmtTime(timezone)}", \
+                tprint(f"\n{status_uploader_name} has a status.\n{gmtTime(timezone)}")
+                wa_bot.send_message(NUMBER, f"{status_uploader_name} has a status.\n{gmtTime(timezone)}", \
                     reply_markup=Inline_list("Show list",list_items=[List_item("Nice one 👌"), List_item("Thanks ✨"), List_item("GGs 🤞")]))
             else:
                 start = reminderFn(time_diff, start)  # Reset time
 
 def open_WhatsApp()-> None:
+
+    wait_until_whatsapp_login_instructions_disappear = lambda: wait3secs.until(EC.visibility_of_element_located((By.XPATH, '//div[@class="_3AjBo"]')))  # WhatsApp list login instructions
 
     bot.get("https://web.whatsapp.com/")
 
@@ -59,22 +61,23 @@ def open_WhatsApp()-> None:
         print(text2art("\nLogging in..."), "💿")
         login_count: int = 1
         while True:
-            try:  # 
+            try:
                 bot.find_element(By.XPATH, '//div[@id="pane-side"]')
                 break
             except NoSuchElementException:  # Log in page (Scan QRCode)
                 with contextlib.suppress(TimeoutException):
-                    wait3secs.until(EC.visibility_of_element_located(
-                        (By.XPATH, '//div[@class="_3AjBo"]')))  # WhatsApp list login instructions
+                    wait_until_whatsapp_login_instructions_disappear()
+
                     if login_count == 1:
                         print(text2art("Please scan the QRCODE to log in"), "🔑")
                         login_count += 1
-                    wait3secs.until(EC.invisibility_of_element(
-                        (By.XPATH, '//div[@class="_3AjBo"]')))  # WhatsApp list login instructions
+
+                    wait_until_whatsapp_login_instructions_disappear()
+
                     wait3secs.until(EC.visibility_of_element_located(
                         (By.XPATH, '//div[@class="_1dEQH _26aja"]'))) # WhatsApp: Text
                     wait3secs.until(EC.invisibility_of_element(
-                        (By.XPATH, '//div[@class="_2dfCc"]')))  # Loading your chat
+                        (By.XPATH, '//div[@class="x1c3i2sq x14ug900 xk82a7y x1sy10c2"]')))  # Loading your chats
                     wait3secs.until(EC.invisibility_of_element(
                         (By.XPATH, '//div[@class="_3HbCE"]'))) # Loading [%]
                     break
@@ -85,9 +88,9 @@ def open_WhatsApp()-> None:
             list_items=[List_item("Nice one 👌"), List_item("Thanks ✨"), List_item("Great Job 🤞")]))
         bot.quit()
 
-def scroll(personName) -> None:
+def scroll(contact_name) -> None:
 
-    bot.find_element(By.XPATH, '//div[@title="Status"]').click()  # Enter Status Screen
+    bot.find_element(By.XPATH, status_list_page_xpath).click()  # Enter Status Screen
     status_container_xpath: str = '//*[@class="g0rxnol2 ggj6brxn m0h2a7mj lb5m6g5c lzi2pvmc ag5g9lrv jhwejjuw ny7g4cd4"]'
 
     vertical_ordinate: int = 0
@@ -96,7 +99,7 @@ def scroll(personName) -> None:
             try:
                 vertical_ordinate += 2500
                 status_container = bot.find_element(By.XPATH, status_container_xpath)
-                viewed_circle_xpath: str = f'//span[@title="{personName}"]//ancestor::div[@class="lhggkp7q ln8gz9je rx9719la"]\
+                viewed_circle_xpath: str = f'//span[@title="{contact_name}"]//ancestor::div[@class="lhggkp7q ln8gz9je rx9719la"]\
                                 //*[local-name()="circle" and @class="j9ny8kmf"]'  # UNVIEWED
                 bot.execute_script(
                     "arguments[0].scrollTop = arguments[1]", status_container, vertical_ordinate)
@@ -104,7 +107,7 @@ def scroll(personName) -> None:
                 bot.execute_script('arguments[0].scrollIntoView();', statusPoster)
                 break
             except NoSuchElementException:
-                viewed_circle_xpath: str = f'//span[@title="{personName}"]//ancestor::div[@class="lhggkp7q ln8gz9je rx9719la"]\
+                viewed_circle_xpath: str = f'//span[@title="{contact_name}"]//ancestor::div[@class="lhggkp7q ln8gz9je rx9719la"]\
                                 //*[local-name()="circle" and @class="i2tfkqu4"]'  # VIEWED
                 statusPoster = bot.find_element(By.XPATH, viewed_circle_xpath)
                 bot.execute_script('arguments[0].scrollIntoView();', statusPoster)
@@ -136,29 +139,30 @@ def checkStatusType(xpath) -> Optional[Dict[str, bool]]:
 
 def autoViewStatus(
     statusTypeMsg: str = "", 
-    statusUploaderName: str = statusUploaderName, 
-    ) -> Optional[Dict[str, str]]:
+    status_uploader_name: str = status_uploader_name, 
+) -> Optional[Dict[str, str]]:
     
     def _backnforward():
         # Go backward and forward to get 'blob' in url
-        bot.find_elements(By.XPATH, barsXpath)[viewed_status-1].click(); sleep(1)
-        bot.find_elements(By.XPATH, barsXpath)[viewed_status].click(); sleep(1)
+        bot.find_elements(By.XPATH, bars_xpath)[viewed_status-1].click(); sleep(1)
+        bot.find_elements(By.XPATH, bars_xpath)[viewed_status].click(); sleep(1)
 
     def click_pause():
         while True:
             try:
                 wait.until(EC.invisibility_of_element_located(
-                    (By.XPATH, '//button[@class="icon-media-disabled"]')))
+                    (By.XPATH, loading_icon_in_status_xpath)))
                 bot.find_element(By.XPATH, pause_btn_xpath).click()
                 sleep(3)
                 break
             except TimeoutException: ...
-            except NoSuchElementException: break  # ALREADY PAUSED
+            except NoSuchElementException: return  # ALREADY PAUSED
 
-    def img_txt_pause():
+    def img_or_txt_pause():
+        """This function pauses when image or text status is detected"""
         try:
             WebDriverWait(bot, 1).until(EC.invisibility_of_element_located(
-                (By.XPATH, '//button[@class="icon-media-disabled"]')))
+                (By.XPATH, loading_icon_in_status_xpath)))
             bot.find_element(By.XPATH, pause_btn_xpath).click()
         except Exception: return
         # except (TimeoutException, NoSuchElementException): return
@@ -169,8 +173,8 @@ def autoViewStatus(
 
     open_WhatsApp()
 
-    search_field = bot.find_element(By.XPATH, '//p[@class="selectable-text copyable-text iq0m558w g0rxnol2"]')
-    search_field.send_keys(statusUploaderName)
+    search_field = bot.find_element(By.XPATH, search_field_xpath)
+    search_field.send_keys(status_uploader_name)
 
     counter = 0
     total_status: int  = 1
@@ -179,32 +183,33 @@ def autoViewStatus(
         while True:
             try:
                 wait.until(EC.invisibility_of_element_located(  # SEARCH BAR LOADING SVG ICON
-                    (By.XPATH, '//*[local-name()="svg" and @class="gdrnme8s hbnrezoj f8mos8ky tkmeqcnu b9fczbqn"]')))
-                bot.find_element(By.XPATH, ppsXpath)  # Status Circle around profile picture
+                    (By.XPATH, search_bar_loading_xpath)))
+                bot.find_element(By.XPATH, pps_xpath)  # Status Circle around profile picture
                 sleep(5)
-                bot.find_element(By.XPATH, ppXpath).click()  # Click Profile Picture to view Status
+                bot.find_element(By.XPATH, pp_xpath).click()  # Click Profile Picture to view Status
                 break
-            except (TimeoutException, NoSuchElementException): continue
+            # NO GREEN CIRCLE AROUND PROFILE PICTURE (NO STATUS YET) OR STILL LOADING SEARCHED WORD
+            except (TimeoutException, NoSuchElementException): continue 
             ######     DEBUGGING PURPOSE     ######
             # except TimeoutException: continue  # STARTING FROM THE
             # except NoSuchElementException:     # TIMEOUTEXECPTION TO
             #     if counter > 1:                # THE LAST BREAK SHOULD
             #         return                     # BE REMOVED CUS IT'S
-            #     scroll(statusUploaderName)     # FOR VIEWING VIEWED STATUS
-            #     wait.until(EC.invisibility_of_element((By.XPATH, tempStatusThumbnail)))
+            #     scroll(status_uploader_name)     # FOR VIEWING VIEWED STATUS
+            #     wait.until(EC.invisibility_of_element((By.XPATH, temp_status_thumbnail)))
             #     bot.find_element(By.XPATH, scrolled_viewed_person_xpath).click()
             #     break
 
-        unviewed_status: int = len(bot.find_elements(By.XPATH, '//div[contains(@class, "mjomr7am")]')) + 1
-        total_status: int = len(bot.find_elements(By.XPATH, barsXpath))
+        unviewed_status: int = len(bot.find_elements(By.XPATH, unviewed_status_xpath)) + 1
+        total_status: int = len(bot.find_elements(By.XPATH, bars_xpath))
         block_line: str = "-"*38
         loop_range: list = range(1, unviewed_status+1)
         viewed_status: int = total_status - unviewed_status
-        statusTypeMsg += f"{statusUploaderName}\nUnviewed Statues " + "is" if unviewed_status == 1 else "are" + f" {unviewed_status} out of {total_status}.\n"
+        statusTypeMsg += f"{status_uploader_name}\nUnviewed Statues " + "is" if unviewed_status == 1 else "are" + f" {unviewed_status} out of {total_status}.\n"
         statusType_xpaths = [img_status_xpath, video_status_xpath, text_status_xpath, audio_status_xpath, oldMessage_status_xpath]
         for status_idx in loop_range:
 
-            img_txt_pause()
+            img_or_txt_pause()
 
             if status_idx == 1: tprint(statusTypeMsg[:-1])
 
@@ -230,20 +235,20 @@ def autoViewStatus(
                         while True:                                
                             with contextlib.suppress(TimeoutException):
                                 try:
-                                    bot.find_element(By.XPATH, barXpath)  # paused status
-                                    style_value = bot.find_element(By.XPATH, barXpath).get_attribute("style")  # Starts @ 100 reduces to 0
+                                    bot.find_element(By.XPATH, paused_video_bar_xpath)  # paused status
+                                    style_value = bot.find_element(By.XPATH, paused_video_bar_xpath).get_attribute("style")  # Starts @ 100 reduces to 0
                                 except NoSuchElementException:
-                                    bot.find_element(By.XPATH, barVA_Xpath)  # playing status
-                                    style_value = bot.find_element(By.XPATH, barVA_Xpath).get_attribute("style")  # Starts @ 100 reduces to 0
+                                    bot.find_element(By.XPATH, playing_video_bar_xpath)  # playing status
+                                    style_value = bot.find_element(By.XPATH, playing_video_bar_xpath).get_attribute("style")  # Starts @ 100 reduces to 0
                                 finally:
                                     video_progress = style_value.split("(")[1].split(")")[0][1:-1]
                                     with contextlib.suppress(TimeoutException, NoSuchElementException):
                                         wait3secs.until(EC.invisibility_of_element_located(
-                                            (By.XPATH, '//button[@class="icon-media-disabled"]')))
+                                            (By.XPATH, loading_icon_in_status_xpath)))
                                         loading_icon = False
                                     if all([(0 <= float(video_progress) <= 30), loading_icon, total_status == 1]): # The only Status
-                                        bot.find_element(By.XPATH, '//span[@data-icon="x-viewer"]').click()
-                                        scroll(statusUploaderName)
+                                        bot.find_element(By.XPATH, status_exit_xpath).click()
+                                        scroll(status_uploader_name)
                                         bot.find_element(By.XPATH, scrolled_viewed_person_xpath).click()
                                     # while not loading_icon:
                                     if all([(0 <= float(video_progress) <= 30), loading_icon, total_status > 1]): # Not the only Status
@@ -283,19 +288,21 @@ def autoViewStatus(
                     check_Status = None
 
                     # Click Next Status
-                    bot.find_elements(By.XPATH, barsXpath)[viewed_status].click()
-                else: # Exit status
+                    bot.find_elements(By.XPATH, bars_xpath)[viewed_status].click()
+                else: # Status view completed, Exit status
                     try:
-                        bot.find_element(By.XPATH, '//span[@data-icon="x-viewer"]').click()
+                        bot.find_element(By.XPATH, status_exit_xpath).click()
                     except NoSuchElementException:
                         sleep(3)
-                        with contextlib.suppress(NoSuchElementException):
-                            bot.find_element(By.XPATH, f'//span[@title="{statusUploaderName}"]//span')
+                        with contextlib.suppress(NoSuchElementException): 
+                            # Check if it has truely exited by confirming if the page is on 'contact_name' search page
+                            # BUT WHAT IF IT HAS NOT? WHY SKIP DO THIS IN THE FIRST PLACE # TODO FIX THIS
+                            bot.find_element(By.XPATH, f'//span[@title="{status_uploader_name}"]//span')
+                            bot.find_element(By.XPATH, '//div[@title="Status"]').send_keys(Keys.ESCAPE) # TODO: Confirm what this does
                     tprint(block_line); sleep(1)
-                    bot.find_element(By.XPATH, '//div[@title="Status"]').send_keys(Keys.ESCAPE)
 
         # Send to Self
-        wa_bot.send_message(NUMBER, f"{statusTypeMsg}\n{statusUploaderName} at {gmtTime(timezone)}.", \
+        wa_bot.send_message(NUMBER, f"{statusTypeMsg}\n{status_uploader_name} at {gmtTime(timezone)}.", \
             reply_markup=Inline_list("Show list",list_items=[List_item("Nice one 👌"), List_item("Thanks ✨"), List_item("Great Job")]))
         statusTypeMsg: str = ""
 
@@ -322,7 +329,7 @@ if __name__ == "__main__":
                     if reminderTime in {1, 2, 3, 4}: break
                 input_count += 1
             break
-
+    
     service = Service(executable_path=driverpath)
     options = Options()
     options.add_argument("--disable-gpu")
--- src/vars.py
+++ src/vars.py
@@ -8,20 +8,26 @@ NUM_ID: str = env_variable.get("NUM_ID")  # Your Number ID
 TOKEN: str =  env_variable.get("TOKEN")  # Token
 CURRENT_DIR = getcwd()
 
+status_uploader_name: str = "contact_name" # As it is saved on your phone(Case Sensitive)
 timezone: str = "Africa/Lagos"  # Your timezone #TODO: Use specified timezone else Automatically get it
+
+driverpath: str = os_path.join(
+    os_path.dirname(os_path.abspath(__file__)),
+    "driver", 
+    "chromedriver.exe"
+)
+
 pause_btn_xpath:str = '//span[@data-icon="status-media-controls-pause"]'
-statusUploaderName: str = "ContactName" # As it is saved on your phone(Case Sensitive)
-ppsXpath: str = f'//span[@title="{statusUploaderName}"]//..//..//..//..//preceding-sibling::\
-    div[@class="_1AHcd"]//*[local-name()="svg" and @class="bx0vhl82 ma4rpf0l lhggkp7q"]'
-ppXpath: str = f'//span[@title="{statusUploaderName}"]//..//..//..//..//preceding-sibling::\
-    div[@class="_1AHcd"]//*[local-name()="svg" and @class="bx0vhl82 ma4rpf0l lhggkp7q"]//parent::div'
-barsXpath: str = '//div[@class="g0rxnol2 qq0sjtgm jxacihee l7jjieqr egv1zj2i ppled2lx gj5xqxfh om6y7gxh"]'
-barXpath: str =  '//div[@class="lhggkp7q qq0sjtgm tkdu00h0 ln8gz9je ppled2lx ss1fofi6 o7z9b2jg"]'
-driverpath: str = os_path.join(CURRENT_DIR, "driver", "chromedriver.exe")
-barVA_Xpath: str =  '//div[@class="lhggkp7q qq0sjtgm tkdu00h0 ln8gz9je ppled2lx ss1fofi6 o7z9b2jg velocity-animating"]'
-scrolled_viewed_person_xpath :str = f'//span[@title="{statusUploaderName}" and \
+pps_xpath: str = f'//span[@title="{status_uploader_name}"]//..//..//..//..//preceding-sibling::\
+    div//*[local-name()="svg"]' # Green circle
+pp_xpath: str = f'//span[@title="{status_uploader_name}"]//..//..//..//..//preceding-sibling::\
+    div//img[contains(@class, "x1hc1fzr _ao3e")]' # Profile picture
+bars_xpath: str = '//div[contains(@class, "x12mruv9 xfs2ol5")]' # All bars in the status(irrespective if viewed or not)
+paused_video_bar_xpath: str =  '//div[contains(@class, "x5i6ehr")]' # paused video
+playing_video_bar_xpath: str = '//div[contains(@class, "x5i6ehr velocity-animating")]' # playing video
+scrolled_viewed_person_xpath :str = f'//span[@title="{status_uploader_name}" and \
     @class="ggj6brxn gfz4du6o r7fjleex g0rxnol2 lhj4utae le5p0ye3 _11JPr"]'
-tempStatusThumbnail: str = f'//span[@title="{statusUploaderName}"]//..//..//..//preceding-sibling::div[@class="_1AHcd"]//\
+temp_status_thumbnail: str = f'//span[@title="{status_uploader_name}"]//..//..//..//preceding-sibling::div[@class="_1AHcd"]//\
     div[@class="t3g6t33p sxl192xd qnwaluaf g9p5wyxn i0tg5vk9 aoogvgrq o2zu3hjb gfz4du6o r7fjleex lniyxyh2 qssinsw9 rx2toazg"]'
 img_status_xpath: str = '//div[@class="g0rxnol2 ln8gz9je ppled2lx gfz4du6o r7fjleex"]//img'
 video_status_xpath: str = '//div[@class="g0rxnol2 ln8gz9je ppled2lx gfz4du6o r7fjleex"]//video'
@@ -31,3 +37,9 @@ oldMessage_status_xpath: str = '//div[contains(@class, "qfejxiq4 b6f1x6w7 m62443
 caption_xpath: str = '//div[@class="tvsr5v2h mz6luxmp clw8hvz5 p2tfx3a3 holukk2e cw3vfol9"]//\
     span[@class="_11JPr" and @dir="auto" and @aria-label]'
 read_more_caption_xpath: str = f'{caption_xpath}//following-sibling::strong'
+search_bar_loading_xpath: str = '//*[local-name()="svg" and @class="x1e112to x1c74tu6 x1esw782 xa4qsjk xhtitgo"]'
+loading_icon_in_status_xpath: str = '//button[@class="icon-media-disabled"]'
+search_field_xpath: str = '//p[@class="selectable-text copyable-text x15bjb6t x1n2onr6"]'
+unviewed_status_xpath: str = '//div[contains(@class, "x5fxwdf")]'
+status_exit_xpath: str = '//span[@data-icon="x-viewer"]'
+status_list_page_xpath: str = '//span[@data-icon="status-outline"]'
